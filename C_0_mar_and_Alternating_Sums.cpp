754fab4319b4a69a7cb85dd2c3e83dda5469797c
Sakshi Naithani <sakshinaithani26@gmail.com>
2026-07-12 21:38:04 +0530

    cf

---
+++ C_0_mar_and_Alternating_Sums.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <iomanip>
+#include <numeric>
+
+using namespace std;
+
+
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define ll long long
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define sz(v) (int)v.size()
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define rrep(i, a, b) for (int i = a; i >= b; --i)
+
+#ifndef ONLINE_JUDGE
+#define dbg(x) cerr << #x << " = " << x << endl
+#else
+#define dbg(x)
+#endif
+
+// Vector I/O
+template<typename T>
+void read(vector<T>& v) { for (auto& x : v) cin >> x; }
+template<typename T>
+void print(const vector<T>& v) { for (auto& x : v) cout << x << " "; cout << endl; }
+
+// Modular Arithmetic
+const int MOD = 1e9 + 7;
+ll power(ll base, ll exp) {
+    ll res = 1;
+    base %= MOD;
+    while (exp > 0) {
+        if (exp % 2 == 1) res = (res * base) % MOD;
+        base = (base * base) % MOD;
+        exp /= 2;
+    }
+    return res;
+}
+int f(int i, int last, vector<int> &arr, int sum, int &ans ){
+    if(sum==0) ans++;
+    if(i==arr.size()) return 0;
+    int a=f(i+1, last, arr, sum, ans);
+    int b=0;
+    if(last==0){
+        b=f(i+1, 1, arr, sum+arr[i], ans);
+    }
+    else {
+        b=f(i+1, 0, arr, sum-arr[i], ans);
+    }
+    return a+b;
+}
+void solve() {
+    // Logic goes here
+    int  k;
+    cin>>k ;
+    vector<int> arr(k);
+    
+        for(int i=0;i<k;i++){
+            cin>>arr[i];
+            
+        }
+        int  ans=0;
+        int l=f(0,0,arr, 0, ans );
+        cout<<ans;
+    
+    
+  
+    cout<<endl;
+    
+    
+}
+
+int main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
